368b1597ec4777969695c3577c7d0f88fe01501e
DiogoS77 <diogo.messi.18@hotmail.com>
2023-07-15 03:10:27 +0100

    postRoutes.js updated get all posts nad post by id

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,34 +1,33 @@
-const router = require('express').Router();
-const { Post } = require('../../models');
+const express = require('express');
+const router = express.Router();
+const { Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+// Get all posts with comments
+router.get('/', withAuth, async (req, res) => {
   try {
-    const newpost = await Post.create({
-      ...req.body,
-      user_id: req.session.user_id,
+    const postData = await Post.findAll({
+      include: [{ model: Comment }],
     });
-
-    res.status(200).json(newpost);
+    res.status(200).json(postData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+// Update a post by ID
+router.put('/:id', withAuth, async (req, res) => {
   try {
-    const postData = await Post.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!postData) {
-      res.status(404).json({ message: 'No post found with this id!' });
-      return;
-    }
-
+    const { title, body } = req.body;
+    const postData = await Post.update(
+      { title, body },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
     res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
