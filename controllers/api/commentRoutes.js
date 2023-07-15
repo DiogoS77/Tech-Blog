819cf704aa2ba22c83b0a87c1e08a2b25e909f82
DiogoS77 <diogo.messi.18@hotmail.com>
2023-07-15 03:38:05 +0100

    renamed projectroute to comment, added post route

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const { body, post_id } = req.body;
+    const newComment = await Comment.create({ body, post_id });
+
+    console.log('Create a new Comment route');
+    res.status(200).json(newComment);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
