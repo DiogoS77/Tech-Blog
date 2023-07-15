d294e2f401133224b2a5848762ae80db97a5689a
DiogoS77 <diogo.messi.18@hotmail.com>
2023-07-15 03:14:35 +0100

    added/updated get single post, create, delete rout

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -34,4 +34,55 @@ router.put('/:id', withAuth, async (req, res) => {
   }
 });
 
+// Get a single post by ID with comments
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: Comment }],
+    });
+
+    if (!postData) {
+      res.status(404).json({ Message: 'No Post found with that id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Create a new post
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const { title, body } = req.body;
+    const user_id = req.session.user_id;
+    const newPost = await Post.create({ title, body, user_id });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Delete a post by ID
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No Post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
