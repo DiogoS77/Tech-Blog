fbffdc41c98f1a0435b29783c3efcba23b29899a
DiogoS77 <diogo.messi.18@hotmail.com>
2023-07-16 02:44:51 +0100

    added and adjusted for nocomment.handlebar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -40,6 +40,23 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
+router.get('/nocomment/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: Comment }, { model: User }],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Profile route - requires authentication
 router.get('/profile', withAuth, async (req, res) => {
   console.log('Profile:', req.session.user_id);
