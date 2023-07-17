069be92d38fe3f2427a12f735abb40555b1c1899
DiogoS77 <diogo.messi.18@hotmail.com>
2023-07-17 01:06:12 +0100

    fixed comments issue , now comments should appear

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,17 +2,18 @@ const submitCommentHandler = async (event) => {
   event.preventDefault();
 
   const comment = document.querySelector('#comment-body').value.trim();
-  const postId = window.location.toString().split('/').slice(-1)[0];
+  const post_id = window.location.toString().split('/').slice(-1)[0];
 
   if (comment) {
     const response = await fetch(`/api/comments`, {
       method: 'POST',
-      body: JSON.stringify({ body: comment }),
+      body: JSON.stringify({ body: comment, post_id }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       console.log('Response OK');
+      document.location.reload();
     } else {
       alert('Failed to add comment.');
     }
